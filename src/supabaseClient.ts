e6d6e13387144996afa3512228de22e1450a8bc1
Shunichi Hirata <pe@ShunichinoMac-mini.local>
2023-11-27 15:45:36 +0900

    デプロイ確認のための出力追加の削除

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -3,10 +3,6 @@ import { createClient } from "@supabase/supabase-js";
 const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
 const SUPABASE_API_KEY = import.meta.env.VITE_SUPABASE_API_KEY;
 
-console.log(`Supabase URL: ${SUPABASE_URL}`);
-// APIキーの存在を確認するだけで、実際の値は出力しない
-console.log(`Supabase API Key is defined: ${!!SUPABASE_API_KEY}`);
-
 if (!SUPABASE_URL || !SUPABASE_API_KEY) {
   throw new Error("Supabase URL and API key must be defined");
 }
