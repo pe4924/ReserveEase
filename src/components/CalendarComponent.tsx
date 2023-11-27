6be80f4593a3b4f63614e94408896d61057505b0
Shunichi Hirata <pe@ShunichinoMac-mini.local>
2023-11-27 23:43:08 +0900

    カレンダーの追加

--- src/components/CalendarComponent.tsx
+++ src/components/CalendarComponent.tsx
@@ -1,11 +1,21 @@
+import { useState } from "react";
+import { DayPicker } from "react-day-picker";
+import { ja } from "date-fns/locale";
+import "react-day-picker/dist/style.css";
+import "../day-picker.css";
+
 const CalendarComponent = () => {
+  const [selected, setSelected] = useState<Date>();
+
   return (
-    <iframe
-      src="https://calendar.google.com/calendar/embed?height=800&wkst=1&bgcolor=%23e0e0e0&ctz=Asia%2FTokyo&showTitle=0&showNav=1&showPrint=0&showTabs=0&showTz=0&showCalendars=0&showDate=1&mode=WEEK&src=c2h1bmljaGlAcGU0OTI0LmNvbQ&src=amEuamFwYW5lc2UjaG9saWRheUBncm91cC52LmNhbGVuZGFyLmdvb2dsZS5jb20&color=%2333B679&color=%23C0CA33"
-      style={{ border: "solid 1px #e0e0e0" }}
-      width="1000"
-      height="800"
-    ></iframe>
+    <div>
+      <DayPicker
+        locale={ja}
+        mode="single"
+        selected={selected}
+        onSelect={setSelected}
+      />
+    </div>
   );
 };
 
