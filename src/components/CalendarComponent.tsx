de335a8a5f474ec95eccdc64bdf5c0e565c43c21
Shunichi Hirata <pe@ShunichinoMac-mini.local>
2023-11-27 22:06:26 +0900

    カレンダーの追加

--- src/components/CalendarComponent.tsx
+++ src/components/CalendarComponent.tsx
@@ -1,111 +1,12 @@
-import { useState, useEffect } from "react";
-
-type Schedule = {
-  id: number;
-  name: string;
-  date: string;
-  startTime: string;
-  endTime: string;
-};
-
 const CalendarComponent = () => {
-  const [schedules, setSchedules] = useState<Schedule[]>([]);
-  const [selectedDate, setSelectedDate] = useState(
-    new Date().toISOString().split("T")[0]
+  return (
+    <iframe
+      src="https://calendar.google.com/calendar/embed?height=800&wkst=1&bgcolor=%23e0e0e0&ctz=Asia%2FTokyo&showTitle=0&showNav=1&showPrint=0&showTabs=0&showTz=0&showCalendars=0&showDate=1&mode=WEEK&src=c2h1bmljaGlAcGU0OTI0LmNvbQ&src=amEuamFwYW5lc2UjaG9saWRheUBncm91cC52LmNhbGVuZGFyLmdvb2dsZS5jb20&color=%2333B679&color=%23C0CA33"
+      style={{ border: "solid 1px #e0e0e0" }}
+      width="1000"
+      height="800"
+    ></iframe>
   );
-
-  useEffect(() => {
-    const dummyData: Schedule[] = [
-      {
-        id: 0,
-        name: "Aさん",
-        date: "2023-11-27",
-        startTime: "10:00",
-        endTime: "17:00",
-      },
-      {
-        id: 1,
-        name: "Bさん",
-        date: "2023-11-27",
-        startTime: "12:00",
-        endTime: "14:00",
-      },
-      // その他のデータ...
-    ];
-    setSchedules(dummyData);
-  }, []);
-
-  const isScheduledTime = (schedule: Schedule, hour: string): boolean => {
-    const scheduleStart = new Date(`${schedule.date}T${schedule.startTime}`);
-    const scheduleEnd = new Date(`${schedule.date}T${schedule.endTime}`);
-    const currentHour = new Date(`${schedule.date}T${hour}`);
-
-    // 予定の開始時刻と終了時刻を30分間隔に合わせる
-    scheduleStart.setMinutes(
-      scheduleStart.getMinutes() - (scheduleStart.getMinutes() % 30)
-    );
-    scheduleEnd.setMinutes(
-      scheduleEnd.getMinutes() + ((30 - (scheduleEnd.getMinutes() % 30)) % 30)
-    );
-
-    return currentHour >= scheduleStart && currentHour < scheduleEnd;
-  };
-
-  const renderCalendar = () => {
-    const hours: string[] = [];
-    for (let hour = 9; hour <= 22; hour++) {
-      hours.push(`${hour}:00`);
-    }
-
-    const filteredSchedules = schedules.filter(
-      (schedule) => schedule.date === selectedDate
-    );
-
-    return (
-      <div className="calendar-container">
-        <input
-          type="date"
-          value={selectedDate}
-          onChange={(e) => setSelectedDate(e.target.value)}
-        />
-        <table className="calendar-table">
-          <thead>
-            <tr>
-              <th></th>
-              {hours.map((hour) => (
-                <th key={hour} className="hour">
-                  {hour}
-                </th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>
-            {filteredSchedules.map((schedule) => (
-              <tr key={schedule.id}>
-                <td>{schedule.name}</td>
-                {hours.map((hour, index) => (
-                  <td
-                    key={hour}
-                    className={
-                      isScheduledTime(schedule, hour) ? "scheduled-time" : ""
-                    }
-                  >
-                    {index % 4 === 0 ? (
-                      ""
-                    ) : (
-                      <div className="quarter-hour"></div>
-                    )}
-                  </td>
-                ))}
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    );
-  };
-
-  return renderCalendar();
 };
 
 export default CalendarComponent;
