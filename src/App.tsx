4562474b44775a84d847a778a18b233459a44b59
Shunichi Hirata <pe@ShunichinoMac-mini.local>
2023-11-27 14:58:37 +0900

    ログイン画面の作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,59 @@
+import React, { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useNavigate,
+} from "react-router-dom";
 import { supabase } from "./supabaseClient";
+import Home from "./Home";
 
 const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginForm />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+};
+
+const LoginForm = () => {
+  const [message, setMessage] = useState("");
+  const [isError, setIsError] = useState(false);
+  const navigate = useNavigate();
+
   const handleSubmit = async (
     event: React.MouseEvent<HTMLButtonElement>,
     action: "signIn" | "signUp"
   ) => {
     event.preventDefault();
+    setMessage("");
+    setIsError(false);
     const form = event.currentTarget.form;
 
     if (form) {
-      // formがnullでないことを確認
       const formData = new FormData(form);
+      let response = null;
+
       if (action === "signIn") {
-        await signIn(formData);
+        response = await signIn(formData);
       } else if (action === "signUp") {
-        await signUp(formData);
+        response = await signUp(formData);
+      }
+
+      if (response && response.error) {
+        setMessage("メールアドレス、またはパスワードが違います");
+        setIsError(true);
+      } else {
+        if (action === "signIn") {
+          navigate("/home");
+        } else {
+          setMessage(
+            "登録が完了しました。送付されたメールを確認して、アカウントの認証を行ってください。"
+          );
+        }
       }
     } else {
       console.error("Form not found");
@@ -25,50 +64,43 @@ const App = () => {
     const email = formData.get("email") as string;
     const password = formData.get("password") as string;
 
-    const { error } = await supabase.auth.signInWithPassword({
+    return await supabase.auth.signInWithPassword({
       email,
       password,
     });
-
-    if (error) {
-      alert("エラー");
-    } else {
-      const currentUser = supabase.auth.getUser();
-      console.log((await currentUser).data.user?.email);
-      alert("成功しました。");
-    }
   };
 
   const signUp = async (formData: FormData) => {
     const email = formData.get("email") as string;
     const password = formData.get("password") as string;
-    const { error } = await supabase.auth.signUp({
+
+    return await supabase.auth.signUp({
       email,
       password,
     });
-
-    if (error) {
-      alert("登録エラー");
-    }
   };
 
   return (
     <div className="container">
-      <nav className="navbar">
-        <div className="navbar-inner">
-          <p className="title">Attendly</p>
+      <nav>
+        <div>
+          <p className="title">Attendly デモVer</p>
         </div>
+        <p className="sub-title">勤怠管理アプリ</p>
       </nav>
       <div className="main-form">
+        {message && (
+          <div className={isError ? "error-message" : "message"}>{message}</div>
+        )}
         <form>
-          <label htmlFor="email">Email</label>
+          <label htmlFor="email">メールアドレス</label>
           <input
             className="form-input"
             name="email"
             placeholder="you@example.com"
             required
           />
-          <label htmlFor="password">Password</label>
+          <label htmlFor="password">パスワード</label>
           <input
             className="form-input"
             type="password"
@@ -80,13 +112,13 @@ const App = () => {
             onClick={(e) => handleSubmit(e, "signIn")}
             className="form-button"
           >
-            Sign In
+            ログイン
           </button>
           <button
             onClick={(e) => handleSubmit(e, "signUp")}
             className="form-button"
           >
-            Sign Up
+            新規登録
           </button>
         </form>
       </div>
